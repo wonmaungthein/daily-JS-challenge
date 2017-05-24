b02709dae88ff28a6d874dd4a65d6fabc3c8cd86
Won Maung Thein <wonmaungthein@gmail.com>
2017-05-24 14:07:18 +0100

    checking and pushing

---
+++ Day1.js
@@ -0,0 +1,81 @@
+
+
+// /*Left tree*/
+
+// var leftTree = "";
+// for (var j = 0; j < 10; j++) {
+//     leftTree += "*";
+//     console.log(leftTree);
+// }
+
+
+/*Right tree */
+
+// var counter = 0;
+// for (var i = 10; i > 0; i--) {
+//     counter++;
+//     var result = "";
+//     for (var j = 0; j < i; j++) {
+//         result += " ";
+//     }
+//     for (var k = counter; k > 0; k--) {
+//         result += "*";
+//     }
+//     console.log(result);
+// }
+
+// /*Third tree */
+
+// var counter = 1;
+// for (var i = 12; i > 0; i--) {
+//     counter = counter + 2;
+//     var result = "";
+//     if (i > 2) {
+
+
+//         for (var j = 0; j < i; j++) {
+//             result += " ";
+//         }
+//         for (var k = counter - 2; k > 0; k--) {
+//             result += "*";
+//         }
+
+//         console.log(result);
+//     }
+//     else {
+//         for (var j = 11; j > 0; j--) {
+//             result += " ";
+//         }
+//         for (var k = 5; k > 2; k--) {
+//             result += "*";
+//         }
+//         console.log(result);
+
+//     }
+
+// // }
+
+// var city = "Glasgow";
+// console.log(city);
+
+// var city = "Rangon";
+// console.log(city);
+
+// var myHomeTown = "Sittway";
+//     myName = "Won Maung Thein";
+//     myAge = "23";
+//     occupation = "Software engineer";
+
+
+// console.log ("I am " + myName + " who is " + myAge + " years old " 
+// + "." + "I was born in " + myHomeTown + " and now I am an " + occupation + ".");
+
+
+// var isHappy = true;
+// if (isHappy === true) {
+//   // if I am happy, print "Hello world!"
+//   console.log('Hello world!');
+// } else {
+//   // if I am sad, print a frowny face
+//   console.log(':(');
+// }
